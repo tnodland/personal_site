d1ec41b6d3dd1d14838281237b101537d93f47d9
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-21 16:26:19 -0700

    Added test user_can_navigate_to_about_test.rb and added html to make it pass

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -3,6 +3,6 @@ require 'pry'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']]
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
   end
 end
