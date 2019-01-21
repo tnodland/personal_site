237cbbc90c01dddaf5f6a7abb6185e65e4761470
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-21 14:04:26 -0700

    Added tmp to gitignore

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -1,7 +1,8 @@
 require 'rack'
+require 'pry'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']] 
+    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']]
   end
 end
--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,6 +4,7 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
+    
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
