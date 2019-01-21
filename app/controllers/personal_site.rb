9a8e2af6820a697c66a8dca1b45598eec62e15f6
Trevor Nodland <trevor.nodland@gmail.com>
2019-01-21 16:28:45 -0700

    Updated personal site to have an error page

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -3,6 +3,18 @@ require 'pry'
 
 class PersonalSite
   def self.call(env)
+    case env["PATH_INFO"]
+    when '/' then index
+    else
+      error
+    end
+  end
+
+  def self.index
     ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def self.error
+    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
   end
 end
